b94d662f77423f2dce2b38be890c079f2cb95c8c
Hangwelani Madilonga <hangwelani3@gmail.com>
2024-01-30 15:34:01 +0200

    Restarting layout from scratch

--- to-do-list/src/App.jsx
+++ to-do-list/src/App.jsx
@@ -5,22 +5,14 @@ import {
   Routes, Route, Link
 } from 'react-router-dom'
 
-import Header from './components/Header'
-import Sidebar from './components/Sidebar'
-import Taskview from './components/Taskview'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-    <>
-        <Header />
-        
-    </>
-    <>
-    <Sidebar />
-    </>
+    
     </>
   )
 }
